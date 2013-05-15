782de5260471e8bc992d6ae7f801dcff8bb0b06f
Luke Mills <luke@aligent.com.au>
2013-05-15 10:52:12 +0930

    Added http_response_send_before observer to add Vary:HTTP_X_GEOIP header for varnish.

---
+++ src/app/code/local/Aligent/GeoIP/Model/Observer.php
@@ -0,0 +1,12 @@
+<?php
+class Aligent_GeoIP_Model_Observer
+{
+
+    /**
+     * Inserts a header into the response to allow Varnish to cache pages based on country
+     */
+    public function setVarnishResponseHeader() {
+        Mage::app()->getResponse()->setHeader('Vary', Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_VARIABLE, true);
+    }
+    
+}
