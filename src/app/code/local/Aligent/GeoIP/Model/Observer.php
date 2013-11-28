cbf5a4f19ae89fa94faa70654935f7ed92164a8f
Jim O'Halloran <jim@aligent.com.au>
2013-11-28 14:08:18 +1030

    Where a store id header waqs supplied, vary the respomse based on that header as well as geoip

--- src/app/code/local/Aligent/GeoIP/Model/Observer.php
+++ src/app/code/local/Aligent/GeoIP/Model/Observer.php
@@ -1,12 +1,27 @@
 <?php
-class Aligent_GeoIP_Model_Observer
-{
+class Aligent_GeoIP_Model_Observer {
 
     /**
-     * Inserts a header into the response to allow Varnish to cache pages based on country
+     * Inserts a header into the response to allow Varnish to cache pages based
+     * on country and store id if appropriate.
      */
     public function setVarnishResponseHeader() {
-        Mage::app()->getResponse()->setHeader('Vary', Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_HEADER, true);
+        $aHeaders = array(Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_HEADER);
+
+        // If Varnish supplies a store id header, vary the response based on the store id.
+        if (array_key_exists('HTTP_X_STOREID', $_SERVER)) {
+            $aHeaders[] = 'X-StoreId';
+        }
+
+        $oResponse = Mage::app()->getResponse();
+        $oResponse->setHeader('Vary', implode(', ', $aHeaders) , true);
+
+//        if (array_key_exists('HTTP_X_STOREID', $_SERVER)) {
+//            $oResponse->setHeader('X-Request-StoreId', $_SERVER['HTTP_X_STOREID']);
+//        }
+//        $oResponse->setHeader('X-Request-GeoIP', $_SERVER[Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_VARIABLE]);
+
+
     }
     
 }
