8bc3da6cc06916e89850a456001c4d57fd76987e
Jim O'Halloran <jim@aligent.com.au>
2013-12-02 15:53:05 +1030

    Comment out debugging code.  Refactor to use constants for StoreId headers.

--- src/app/code/local/Aligent/GeoIP/Model/Observer.php
+++ src/app/code/local/Aligent/GeoIP/Model/Observer.php
@@ -1,5 +1,7 @@
 <?php
 class Aligent_GeoIP_Model_Observer {
+    const VARNISH_STOREID_SERVER_VARIABLE = 'HTTP_X_STOREID';
+    const VARNISH_STOREID_HEADER = 'X-StoreId';
 
     /**
      * Inserts a header into the response to allow Varnish to cache pages based
@@ -9,19 +11,20 @@ class Aligent_GeoIP_Model_Observer {
         $aHeaders = array(Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_HEADER);
 
         // If Varnish supplies a store id header, vary the response based on the store id.
-        if (array_key_exists('HTTP_X_STOREID', $_SERVER)) {
-            $aHeaders[] = 'X-StoreId';
+        if (array_key_exists(self::VARNISH_STOREID_SERVER_VARIABLE, $_SERVER)) {
+            $aHeaders[] = self::VARNISH_STOREID_HEADER;
         }
 
         $oResponse = Mage::app()->getResponse();
         $oResponse->setHeader('Vary', implode(', ', $aHeaders) , true);
 
-//        if (array_key_exists('HTTP_X_STOREID', $_SERVER)) {
-//            $oResponse->setHeader('X-Request-StoreId', $_SERVER['HTTP_X_STOREID']);
-//        }
-//        $oResponse->setHeader('X-Request-GeoIP', $_SERVER[Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_VARIABLE]);
-
-
+        // Put the request sotre id and country into server vars.  Useful for debugging.
+        //if (array_key_exists(self::VARNISH_STORE_ID_HEADER, $_SERVER)) {
+        //    $oResponse->setHeader('X-Request-StoreId', $_SERVER[self::VARNISH_STORE_ID_HEADER]);
+        //}
+        //if (array_key_exists(Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_VARIABLE, $_SERVER)) {
+        //    $oResponse->setHeader('X-Request-GeoIP', $_SERVER[Aligent_GeoIP_Helper_Data::VARNISH_XGEOIP_SERVER_VARIABLE]);
+        //}
     }
     
 }
