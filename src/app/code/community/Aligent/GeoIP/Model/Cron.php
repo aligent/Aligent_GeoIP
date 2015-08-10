e391c6873d414d5a4b8ca44d3bee5ba49c0cf405
phirunson <phirun@aligent.com.au>
2015-08-10 11:00:50 +0930

    Fix infinite loop issue when update files could not be opened/created

--- src/app/code/community/Aligent/GeoIP/Model/Cron.php
+++ src/app/code/community/Aligent/GeoIP/Model/Cron.php
@@ -2,6 +2,8 @@
 
 class Aligent_GeoIP_Model_Cron {
 
+    const LOG_FILE = 'aligent/geoip.log';
+
     public function runUpdateGeoip() {
         $this->_runUpdate('aligent_geoip/geoip/autoupdate_geoip', 'GeoIP.dat.gz');
     }
@@ -28,6 +30,14 @@ class Aligent_GeoIP_Model_Cron {
         // Open our files (in binary mode)
         $oFile = gzopen($vFileName, 'rb');
         $oOutFile = fopen($vOutFilename, 'wb');
+
+        //if either input or output file could not be opened
+        if ($oFile === false || $oOutFile === false) {
+            //Unable to open file/update. Log error and return
+            Mage::log('Unable to update GeoIP data', null, self::LOG_FILE, true);
+            return;
+        }
+
         // Keep repeating until the end of the input file
         while(!gzeof($oFile)) {
             // Read buffer-size bytes
